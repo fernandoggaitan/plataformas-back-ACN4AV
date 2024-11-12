329b59437e9ac07f9a55ced3c250228da04c1bde
fernandoggaitan <fernandoggaitan@gmail.com>
2024-11-12 20:54:21 -0300

    Access toke / Middleware

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,5 +1,7 @@
 const usuarioModel = require("../models/usuarioModel");
 
+const jwt = require('jsonwebtoken');
+
 exports.register = async(req, res) => {
     const {nombre, email, contrasena} = req.body;
     const is_admin = false;
@@ -19,15 +21,32 @@ exports.login = async(req, res) => {
         if(usuario == null){
             res.json({ success: false, message: 'Credenciales incorrectas' });
         }else{
+
+            const playload = { 
+                ID: usuario.id, 
+                nombre: usuario.nombre, 
+                is_admin: (usuario.is_admin == '1') 
+            };
+
+            //Access token.
+            const accessToken = jwt.sign(playload, process.env.JWT_ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+
             res.json({
                 success: true,
                 message: 'Inicio de sesión exitoso',
                 nombre: usuario.nombre,
-                ID: usuario.id
+                ID: usuario.id,
+                accessToken
             });
+
         }
     }catch(error){
         console.log(error);
         res.status(500).json({ success: false, message: 'Error al intentar iniciar sesión' });
     }
+}
+
+//Ruta protegida
+exports.welcome = (req, res) => {
+    res.json({ success: true, message: 'Bienvenida/o ' + req.user.nombre });
 }
